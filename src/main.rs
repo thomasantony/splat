23a16fefa1564e8a547734eef771cf6d674873a9
Thomas Antony <mail@thomasantony.com>
2023-11-10 20:29:11 -0800

    Add parser for Gaussian structs

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,143 @@
+use ply::parser;
+use ply::ply::Property;
 use ply_rs as ply;
 use ply_rs::ply::{ Ply, DefaultElement };
 use ply_rs::writer::{ Writer };
 
-// A struct for holding parameters of a 3D gaussian
-#[derive(Debug)]
-struct Gaussian {
-    mean: [f64; 3],
-    variance: [f64; 3],
+use nalgebra as na;
+
+#[derive(Clone, Debug)]
+pub struct Gaussian {
+    pub position: na::Point3<f32>,  // x, y, z
+    pub scale: na::Vector3<f32>,                // scale_0, scale_1, scale_2
+    pub opacity: f32,               // opacity
+    pub rotation: na::Quaternion<f32>,  // rot_0, rot_1, rot_2, rot_3
+    pub color: na::Vector3<f32>,    // f_dc_0, f_dc_1, f_dc_2
+    pub sh: na::SVector<f32, 45>,   // f_rest_0 ... f_rest_44
 }
 
-fn extract_three_points(ply: &Ply<DefaultElement>)
-{
-      // Copy the first three splats into a vector and then write into a new PLY file
-    let (_, points) = ply.payload.front().unwrap();
-    let mut output_points = Vec::new();
-    for (i, splat) in points.iter().enumerate()
-    {
-        if i > 2
-        {
-            break;
+impl ply::ply::PropertyAccess for Gaussian {
+    fn new() -> Self {
+        Self {
+            position: na::Point3::new(0.0, 0.0, 0.0),
+            scale: na::Vector3::new(0.0, 0.0, 0.0),
+            opacity: 0.0,
+            rotation: na::Quaternion::identity(),
+            color: na::Vector3::new(0.0, 0.0, 0.0),
+            sh: na::SVector::<f32, 45>::zeros(),
         }
-        output_points.push(splat.clone());
     }
-    // Append the splats to the new PLY file
-    let mut output_ply = Ply::<DefaultElement>::new();
-    output_ply.payload.insert("vertex".to_string(), output_points);
-    // Set header to match the original
-    output_ply.header = ply.header.clone();
-
-    // set up a writer
-    let mut outfile = std::fs::File::create("simple2.ply").unwrap();
-    let w = Writer::new();
-    let written = w.write_ply(&mut outfile, &mut output_ply).unwrap();
-    println!("Wrote {} bytes", written);
+    // Set properties from the PLY file
+    fn set_property(&mut self, property_name: String, property: Property)
+    {
+        //Assign values based on property name
+        match (property_name.as_ref(), property) {
+            ("x", ply::ply::Property::Float(v)) => self.position[0] = v,
+            ("y", ply::ply::Property::Float(v)) => self.position[1] = v,
+            ("z", ply::ply::Property::Float(v)) => self.position[2] = v,
+            ("scale_0", ply::ply::Property::Float(v)) => self.scale[0] = v,
+            ("scale_1", ply::ply::Property::Float(v)) => self.scale[1] = v,
+            ("scale_2", ply::ply::Property::Float(v)) => self.scale[2] = v,
+            ("opacity", ply::ply::Property::Float(v)) => self.opacity = v,
+            ("rot_0", ply::ply::Property::Float(v)) => self.rotation[0] = v,
+            ("rot_1", ply::ply::Property::Float(v)) => self.rotation[1] = v,
+            ("rot_2", ply::ply::Property::Float(v)) => self.rotation[2] = v,
+            ("rot_3", ply::ply::Property::Float(v)) => self.rotation[3] = v,
+            ("f_dc_0", ply::ply::Property::Float(v)) => self.color[0] = v,
+            ("f_dc_1", ply::ply::Property::Float(v)) => self.color[1] = v,
+            ("f_dc_2", ply::ply::Property::Float(v)) => self.color[2] = v,
+            (s, ply::ply::Property::Float(v)) if s.starts_with("f_rest_") => {
+                let index = s[7..].parse::<usize>().unwrap();
+                self.sh[index] = v;
+            }
+            _ => {}
+        }
+    }
 }
+
+// Array of structures (AoS) representation of 3D gaussians with spherical harmonic coefficients
+#[derive(Clone, Debug)]
+pub struct GaussianCollection
+{
+    pub positions: Vec<na::Point3<f32>>,
+    pub scales: Vec<na::Vector3<f32>>,
+    pub opacities: Vec<f32>,
+    pub rotations: Vec<na::UnitQuaternion<f32>>,
+    pub sh: Vec<na::SVector<f32, 45>>,
+}
+impl GaussianCollection{
+    pub fn new() -> Self
+    {
+        Self{
+            positions: Vec::new(),
+            scales: Vec::new(),
+            opacities: Vec::new(),
+            rotations: Vec::new(),
+            sh: Vec::new(),
+        }
+    }
+    pub fn extend(&mut self, other: &Vec<Gaussian>)
+    {
+        for gaussian in other
+        {
+            self.push(gaussian);
+        }
+    }
+    pub fn push(&mut self, gaussian: &Gaussian)
+    {
+        self.positions.push(gaussian.position);
+        self.scales.push(gaussian.scale);
+        self.opacities.push(gaussian.opacity);
+        self.rotations.push(na::UnitQuaternion::from_quaternion(gaussian.rotation));
+        self.sh.push(gaussian.sh);
+    }
+}
+
+pub struct Covariance3DCollection(pub Vec<na::SMatrix<f32, 3, 3>>);
+
+
+impl GaussianCollection{
+
+    pub fn compute_covariance_3D(&self) -> Covariance3DCollection
+    {
+        let mut covariances = Vec::new();
+        for i in 0..self.positions.len()
+        {
+
+            let rotation = self.rotations[i].to_rotation_matrix();
+            // square the scales and make a diagonal matrix
+            let scale = na::Matrix3::new(
+                self.scales[i][0] * self.scales[i][0], 0.0, 0.0,
+                0.0, self.scales[i][1] * self.scales[i][1], 0.0,
+                0.0, 0.0, self.scales[i][2] * self.scales[i][2],
+            );
+            let covariance = rotation.transpose() * scale * rotation;
+            covariances.push(rotation * covariance * rotation.transpose());
+        }
+        Covariance3DCollection(covariances)
+    }
+}
+
 fn main() {
-    // Open ply file at sparse/0/points3D.ply
-    // let mut file = std::fs::File::open("output/point_cloud/iteration_30000/point_cloud.ply").unwrap();
-    // let mut file = std::fs::File::open("simple.ply").unwrap();
-    let mut file = std::fs::File::open("garden.splat").unwrap();
-    let p = ply::parser::Parser::<ply::ply::DefaultElement>::new();
 
-    // use the parser: read the entire file
-    let ply = p.read_ply(&mut file);
+    // Parse payload into a GaussianCollection
+    let mut gaussians = GaussianCollection::new();
+    let file = std::fs::File::open("simple2.ply").unwrap();
+    let mut file = std::io::BufReader::new(file);
+    let gaus_parser = parser::Parser::<Gaussian>::new();
+    let header = gaus_parser.read_header(&mut file).unwrap();
 
-    // make sure it did work
-    assert!(ply.is_ok());
-    let ply = ply.unwrap();
+    // Depending on the header, read the data into our structs..
+    for (_ignore_key, element) in &header.elements {
+        // we could also just parse them in sequence, but the file format might change
+        match element.name.as_ref() {
+            "vertex" => {
+                let gaus = gaus_parser.read_payload_for_element(&mut file, &element, &header).unwrap();
+                println!("Gaussians: {:#?}", gaus);
+                gaussians.extend(&gaus);
 
-    println!("PLY header: {:#?}", ply.header);
-    println!("PLY payload: {:#?}", ply.payload);
-    // extract_three_points(&ply);
-
-    // x, y, z, nx, ny, nz
-    // f_dc_0, f_dc_1, f_dc_2
-    // f_rest_0 ... f_rest_44
-    // scale_0, scale_1, scale_2
-    // opacity,
-    // rot_0, rot_1, rot_2, rot_3
+            },
+            _ => panic!("Unexpected element!"),
+        }
+    }
 }
