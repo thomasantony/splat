1946d77685b21c9c6437e19e1fdea07724053fb6
Thomas Antony <mail@thomasantony.com>
2023-11-12 18:24:59 -0800

    Clean up notebook and add support for higher res output

--- notes/util.py
+++ notes/util.py
@@ -1,3 +1,4 @@
+# Adapted from the version at https://github.com/limacv/GaussianSplattingViewer/blob/main/util.py
 from OpenGL.GL import *
 import OpenGL.GL.shaders as shaders
 import numpy as np
@@ -5,14 +6,14 @@ import glm
 import ctypes
 
 class Camera:
-    def __init__(self, h, w):
+    def __init__(self, h, w, position=(0.0, 0.0, 3.0), target=(0.0, 0.0, 0.0)):
         self.znear = 0.01
         self.zfar = 100
         self.h = h
         self.w = w
-        self.fovy = np.pi / 2
-        self.position = np.array([0.0, 0.0, 3.0])
-        self.target = np.array([0.0, 0.0, 0.0])
+        self.fovy = np.pi / 2.0
+        self.position = np.array(position)
+        self.target = np.array(target)
         self.up = np.array([0.0, -1.0, 0.0])
         self.yaw = -np.pi / 2
         self.pitch = 0
@@ -95,9 +96,15 @@ class Camera:
         proj_mat = self.get_projection_matrix()
         return np.linalg.inv(proj_mat) @ points
 
-    def ndc_to_pixel(self, points_ndc):
-        width_half = self.w / 2
-        height_half = self.h / 2
+    def ndc_to_pixel(self, points_ndc, screen_width=None, screen_height=None):
+        # Use camera plane size if screen size not specified
+        if screen_width is None:
+            screen_width = self.w
+        if screen_height is None:
+            screen_height = self.h
+
+        width_half = screen_width / 2
+        height_half = screen_height / 2
 
         if len(points_ndc.shape) == 1:
             # It is a single point, so just return the pixel coordinates
@@ -106,211 +113,7 @@ class Camera:
             return np.array([(point[0] * width_half + width_half, - point[1] * height_half + height_half)
                 for point in points_ndc])
 
-    def cam_to_pixel(self, points):
-        points_ndc = self.cam_to_ndc(points)
-        return self.ndc_to_pixel(points_ndc)
-
-    def pixel_to_cam(self, points):
-        points_ndc = self.pixel_to_ndc(points)
-        return self.ndc_to_cam(points_ndc)
-
     def update_resolution(self, height, width):
         self.h = height
         self.w = width
         self.is_intrin_dirty = True
-
-
-def load_shaders(vs, fs):
-    vertex_shader = open(vs, 'r').read()
-    fragment_shader = open(fs, 'r').read()
-
-    active_shader = shaders.compileProgram(
-        shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
-        shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER),
-    )
-    return active_shader
-
-def set_attributes(program, keys, values, vao=None, buffer_ids=None):
-    glUseProgram(program)
-    if vao is None:
-        vao = glGenVertexArrays(1)
-    glBindVertexArray(vao)
-
-    if buffer_ids is None:
-        buffer_ids = [None] * len(keys)
-    for i, (key, value, b) in enumerate(zip(keys, values, buffer_ids)):
-        if b is None:
-            b = glGenBuffers(1)
-            buffer_ids[i] = b
-        glBindBuffer(GL_ARRAY_BUFFER, b)
-        glBufferData(GL_ARRAY_BUFFER, value.nbytes, value.reshape(-1), GL_STATIC_DRAW)
-        length = value.shape[-1]
-        pos = glGetAttribLocation(program, key)
-        glVertexAttribPointer(pos, length, GL_FLOAT, False, 0, None)
-        glEnableVertexAttribArray(pos)
-
-    glBindBuffer(GL_ARRAY_BUFFER,0)
-    return vao, buffer_ids
-
-def set_attribute(program, key, value, vao=None, buffer_id=None):
-    glUseProgram(program)
-    if vao is None:
-        vao = glGenVertexArrays(1)
-    glBindVertexArray(vao)
-
-    if buffer_id is None:
-        buffer_id = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, buffer_id)
-    glBufferData(GL_ARRAY_BUFFER, value.nbytes, value.reshape(-1), GL_STATIC_DRAW)
-    length = value.shape[-1]
-    pos = glGetAttribLocation(program, key)
-    glVertexAttribPointer(pos, length, GL_FLOAT, False, 0, None)
-    glEnableVertexAttribArray(pos)
-    glBindBuffer(GL_ARRAY_BUFFER,0)
-    return vao, buffer_id
-
-def set_attribute_instanced(program, key, value, instance_stride=1, vao=None, buffer_id=None):
-    glUseProgram(program)
-    if vao is None:
-        vao = glGenVertexArrays(1)
-    glBindVertexArray(vao)
-
-    if buffer_id is None:
-        buffer_id = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, buffer_id)
-    glBufferData(GL_ARRAY_BUFFER, value.nbytes, value.reshape(-1), GL_STATIC_DRAW)
-    length = value.shape[-1]
-    pos = glGetAttribLocation(program, key)
-    glVertexAttribPointer(pos, length, GL_FLOAT, False, 0, None)
-    glEnableVertexAttribArray(pos)
-    glVertexAttribDivisor(pos, instance_stride)
-    glBindBuffer(GL_ARRAY_BUFFER,0)
-    return vao, buffer_id
-
-def set_storage_buffer_data(program, key, value: np.ndarray, bind_idx, vao=None, buffer_id=None):
-    glUseProgram(program)
-    # if vao is None:  # TODO: if this is really unnecessary?
-    #     vao = glGenVertexArrays(1)
-    if vao is not None:
-        glBindVertexArray(vao)
-
-    if buffer_id is None:
-        buffer_id = glGenBuffers(1)
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, buffer_id)
-    glBufferData(GL_SHADER_STORAGE_BUFFER, value.nbytes, value.reshape(-1), GL_STATIC_DRAW)
-    # pos = glGetProgramResourceIndex(program, GL_SHADER_STORAGE_BLOCK, key)  # TODO: ???
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, bind_idx, buffer_id)
-    # glShaderStorageBlockBinding(program, pos, pos)  # TODO: ???
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
-
-def set_faces_tovao(vao, faces: np.ndarray):
-    # faces
-    glBindVertexArray(vao)
-    element_buffer = glGenBuffers(1)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces.nbytes, faces, GL_STATIC_DRAW)
-    return element_buffer
-
-def set_gl_bindings(vertices, faces):
-    # vertices
-    vao = glGenVertexArrays(1)
-    glBindVertexArray(vao)
-    # vertex_buffer = glGenVertexArrays(1)
-    vertex_buffer = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
-    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-    glVertexAttribPointer(0, 4, GL_FLOAT, False, 0, None)
-    glEnableVertexAttribArray(0)
-
-    # faces
-    element_buffer = glGenBuffers(1)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces.nbytes, faces, GL_STATIC_DRAW)
-    # glVertexAttribPointer(1, 3, GL_FLOAT, False, 36, ctypes.c_void_p(12))
-    # glEnableVertexAttribArray(1)
-    # glVertexAttribPointer(2, 3, GL_FLOAT, False, 36, ctypes.c_void_p(12))
-    # glEnableVertexAttribArray(2)
-
-def set_uniform_mat4(shader, content, name):
-    glUseProgram(shader)
-    if isinstance(content, glm.mat4):
-        content = np.array(content).astype(np.float32)
-    else:
-        content = content.T
-    glUniformMatrix4fv(
-        glGetUniformLocation(shader, name),
-        1,
-        GL_FALSE,
-        content.astype(np.float32)
-    )
-
-def set_uniform_1f(shader, content, name):
-    glUseProgram(shader)
-    glUniform1f(
-        glGetUniformLocation(shader, name),
-        content,
-    )
-
-def set_uniform_1int(shader, content, name):
-    glUseProgram(shader)
-    glUniform1i(
-        glGetUniformLocation(shader, name),
-        content
-    )
-
-def set_uniform_v3f(shader, contents, name):
-    glUseProgram(shader)
-    glUniform3fv(
-        glGetUniformLocation(shader, name),
-        len(contents),
-        contents
-    )
-
-def set_uniform_v3(shader, contents, name):
-    glUseProgram(shader)
-    glUniform3f(
-        glGetUniformLocation(shader, name),
-        contents[0], contents[1], contents[2]
-    )
-
-def set_uniform_v1f(shader, contents, name):
-    glUseProgram(shader)
-    glUniform1fv(
-        glGetUniformLocation(shader, name),
-        len(contents),
-        contents
-    )
-
-def set_uniform_v2(shader, contents, name):
-    glUseProgram(shader)
-    glUniform2f(
-        glGetUniformLocation(shader, name),
-        contents[0], contents[1]
-    )
-
-def set_texture2d(img, texid=None):
-    h, w, c = img.shape
-    assert img.dtype == np.uint8
-    if texid is None:
-        texid = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, texid)
-    glTexImage2D(
-        GL_TEXTURE_2D, 0, GL_RGB, w, h, 0,
-        GL_RGB, GL_UNSIGNED_BYTE, img
-    )
-    glActiveTexture(GL_TEXTURE0)  # can be removed
-    # glGenerateMipmap(GL_TEXTURE_2D)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
-    return texid
-
-def update_texture2d(img, texid, offset):
-    x1, y1 = offset
-    h, w = img.shape[:2]
-    glBindTexture(GL_TEXTURE_2D, texid)
-    glTexSubImage2D(
-        GL_TEXTURE_2D, 0, x1, y1, w, h,
-        GL_RGB, GL_UNSIGNED_BYTE, img
-    )
